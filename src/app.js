fb84a8898e7ce11b3a583ddefee7b165e1501e4e
wanghongfei <947922098@qq.com>
2020-05-16 00:11:19 +0800

    first

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Button} from "antd";
+
+export default class App extends React.Component{
+    render() {
+        return(
+            <div>
+               <Button type='primary' >hello</Button>
+            </div>
+        )
+    }
+}
